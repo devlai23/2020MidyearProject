da4d428770df76fa56d54e336f4960a538a9ac90
Emilio L. Aleman <emiliolaleman@gmail.com>
2021-01-15 13:08:33 -0500

    printed board

--- javafx/src/gui_board.java
+++ javafx/src/gui_board.java
@@ -11,23 +11,40 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
-public class gui_board {
+public class gui_board extends Application {
 
     public void start(Stage primaryStage) {
         Pane pane = new Pane();
+        // see if i need more layers when adding checkers
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
+                Rectangle rect = new Rectangle();
 
+                rect.widthProperty().bind(pane.widthProperty().divide(8));
+                rect.heightProperty().bind(pane.heightProperty().divide(8));
+                rect.xProperty().bind(pane.widthProperty().divide(8).multiply(c));
+                rect.yProperty().bind(pane.heightProperty().divide(8).multiply(r));
 
+                rect.setFill(Color.BLACK);
+                if ((r+c) % 2 == 0) {
+                    rect.setFill(Color.WHITE);
+                }
+                pane.getChildren().add(rect);
+            }
+        }
 
-        Scene scene = new Scene(null, 300, 200);
+        Scene scene = new Scene(pane, 600, 600);
         primaryStage.setTitle("Checkers: Java");
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        EventHandler<ActionEvent> eventHandler = e -> {
+        // EventHandler<ActionEvent> eventHandler = e -> {
             
-        };
+        // };
     }
+
 }
